ce6cbd94c19f0e4191c2367aec3876c9537b45dd
iamtong <jiantong912@163.com>
2019-12-06 13:28:11 +0800

    fix debug

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,3 @@
 <?php
 
-Route::post('_grid-sortable_', \iamtong\LaravelAdmin\GridSortable\Http\GridSortableController::class)->name('laravel-admin-grid-sortable');
+Route::post('_grid-sortable_', "iamtong\LaravelAdmin\GridSortable\Http\GridSortableController@update")->name('laravel-admin-grid-sortable');
